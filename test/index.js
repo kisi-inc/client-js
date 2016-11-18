86b08d4830bfac5f42aeb6957e7d57fee292c41b
Carl <ce07c3@gmail.com>
2016-11-18 12:31:54 +0100

    Release 5.0.0

--- test/index.js
+++ test/index.js
@@ -1,215 +1,213 @@
-import { assert } from "chai"
-import axios from "axios"
-import AxiosMockAdapter from "axios-mock-adapter"
-import Kisi from "../lib/index"
+import { assert } from 'chai';
+import AxiosMockAdapter from 'axios-mock-adapter';
+import Kisi from '../lib/index';
 
-const kisiClient = new Kisi()
+const kisiClient = new Kisi();
 
-const axiosMockAdapter = new AxiosMockAdapter(kisiClient.client)
+const axiosMockAdapter = new AxiosMockAdapter(kisiClient.client);
 
-describe("sign up, in and out", function () {
-    it("should sign up", function (done) {
-        axiosMockAdapter
+/* global describe it */
+describe('sign up, in and out', () => {
+  it('should sign up', (done) => {
+    axiosMockAdapter
             .onPost(/users$/)
             .reply(200, {
-                email: "test@kisi.io"
-            }, {})
+              email: 'test@kisi.io',
+            }, {});
 
-        kisiClient
-            .signUp("test@kisi.io", "test")
+    kisiClient
+            .signUp('test@kisi.io', 'test')
             .then((result) => {
-                assert.deepEqual(result, { email: "test@kisi.io" })
+              assert.deepEqual(result, { email: 'test@kisi.io' });
 
-                done()
+              done();
             })
-            .catch(error => done(error))
-    })
+            .catch(error => done(error));
+  });
 
-    it("should sign in", function (done) {
-        axiosMockAdapter
+  it('should sign in', (done) => {
+    axiosMockAdapter
             .onPost(/logins$/)
-            .reply(200, {
-                secret: "secret",
-            }, {})
+            .reply(200, { secret: 'secret' }, {});
 
-        kisiClient
-            .signIn("test@kisi.io", "test")
+    kisiClient
+            .signIn('test@kisi.io', 'test')
             .then((result) => {
-                assert.deepEqual(result, { secret: "secret" })
-                assert.strictEqual(kisiClient
+              assert.deepEqual(result, { secret: 'secret' });
+              assert.strictEqual(kisiClient
                     .client
                     .defaults
                     .headers
-                    .common["X-Login-Secret"], "secret")
+                    .common['X-Login-Secret'], 'secret');
 
-                done()
+              done();
             })
-            .catch(error => done(error))
-    })
+            .catch(error => done(error));
+  });
 
-    it("should sign out", function (done) {
-        axiosMockAdapter
+  it('should sign out', (done) => {
+    axiosMockAdapter
             .onDelete(/login$/)
-            .reply(204, null, {})
+            .reply(204, null, {});
 
-        kisiClient
+    kisiClient
             .signOut()
             .then((result) => {
-                assert.strictEqual(result, null)
-                assert.strictEqual(kisiClient
+              assert.strictEqual(result, null);
+              assert.strictEqual(kisiClient
                     .client
                     .defaults
                     .headers
-                    .common["X-Login-Secret"], null)
+                    .common['X-Login-Secret'], null);
 
-                done()
+              done();
             })
-            .catch(error => done(error))
-    })
-})
+            .catch(error => done(error));
+  });
+});
 
-describe("CRUD", function () {
-    it("should get", function (done) {
-        axiosMockAdapter
+describe('CRUD', () => {
+  it('should get', (done) => {
+    axiosMockAdapter
             .onGet(/get$/)
-            .reply(200, {
-                id: 1
-            }, {})
+            .reply(200, { id: 1 }, {});
 
-        kisiClient
-            .get("get", { id: 1 })
+    kisiClient
+            .get('get', { id: 1 })
             .then((result) => {
-                assert.strictEqual(result.id, 1)
+              assert.strictEqual(result.id, 1);
 
-                done()
+              done();
             })
-            .catch(error => done(error))
-    })
+            .catch(error => done(error));
+  });
 
-    it("should get multiple", function (done) {
-        axiosMockAdapter
+  it('should get multiple', (done) => {
+    axiosMockAdapter
             .onGet(/getMultiple$/)
-            .reply(200, [{}], { "x-collection-range": "0-0/1" })
+            .reply(200, [{}], { 'x-collection-range': '0-0/2' });
 
-        kisiClient
-            .get("getMultiple")
+    kisiClient
+            .get('getMultiple')
             .then((result) => {
-                assert.deepEqual(result, {
-                    pagination: {
-                        offset: 0,
-                        limit: 1
-                    },
-                    data: [
-                        {}
-                    ]
-                })
+              assert.deepEqual(result, {
+                pagination: {
+                  offset: 0,
+                  limit: 1,
+                  count: 2,
+                },
+                data: [
+                        {},
+                ],
+              });
 
-                done()
+              done();
             })
-            .catch(error => done(error))
-    })
+            .catch(error => done(error));
+  });
 
-    it("should get empty multiple", function (done) {
-        axiosMockAdapter
+  it('should get empty multiple', (done) => {
+    axiosMockAdapter
             .onGet(/getEmptyMultiple$/)
-            .reply(200, [], { "x-collection-range": "*/0" })
+            .reply(200, [], { 'x-collection-range': '*/0' });
 
-        kisiClient
-            .get("getEmptyMultiple")
+    kisiClient
+            .get('getEmptyMultiple')
             .then((result) => {
-                assert.deepEqual(result, {
-                    pagination: {
-                        offset: 0,
-                        limit: 0
-                    },
-                    data: []
-                })
+              assert.deepEqual(result, {
+                pagination: {
+                  offset: 0,
+                  limit: 0,
+                  count: 0,
+                },
+                data: [],
+              });
 
-                done()
+              done();
             })
-            .catch(error => done(error))
-    })
+            .catch(error => done(error));
+  });
 
-    it("should post", function (done) {
-        axiosMockAdapter
+  it('should post', (done) => {
+    axiosMockAdapter
             .onGet(/post$/)
             .reply(200, {
-                id: 1
-            }, {})
+              id: 1,
+            }, {});
 
-        kisiClient
-            .get("post", { id: 1 })
+    kisiClient
+            .get('post', { id: 1 })
             .then((result) => {
-                assert.deepEqual(result, { id: 1 })
+              assert.deepEqual(result, { id: 1 });
 
-                done()
+              done();
             })
-            .catch(error => done(error))
-    })
+            .catch(error => done(error));
+  });
 
-    it("should put", function (done) {
-        axiosMockAdapter
+  it('should put', (done) => {
+    axiosMockAdapter
             .onGet(/put$/)
-            .reply(204, null, {})
+            .reply(204, null, {});
 
-        kisiClient
-            .get("put", { id: 1 })
+    kisiClient
+            .get('put', { id: 1 })
             .then((result) => {
-                assert.strictEqual(result, null)
+              assert.strictEqual(result, null);
 
-                done()
+              done();
             })
-            .catch(error => done(error))
-    })
+            .catch(error => done(error));
+  });
 
-    it("should delete", function (done) {
-        axiosMockAdapter
+  it('should delete', (done) => {
+    axiosMockAdapter
             .onGet(/delete$/)
-            .reply(204, null, {})
+            .reply(204, null, {});
 
-        kisiClient
-            .get("delete")
+    kisiClient
+            .get('delete')
             .then((result) => {
-                assert.strictEqual(result, null)
+              assert.strictEqual(result, null);
 
-                done()
+              done();
             })
-            .catch(error => done(error))
-    })
+            .catch(error => done(error));
+  });
 
-    it("should fail", function (done) {
-        axiosMockAdapter
+  it('should fail', (done) => {
+    axiosMockAdapter
             .onGet(/fail$/)
             .reply(401, {
-                code: "abc123",
-                error: "Not authorized"
-            }, {})
+              code: 'abc123',
+              error: 'Not authorized',
+            }, {});
 
-        kisiClient
-            .get("fail")
-            .then(() => done(new Error("Should not happen")))
-            .catch(error => {
-                assert.strictEqual(error.status, 401)
-                assert.strictEqual(error.code, "abc123")
-                assert.strictEqual(error.reason, "Not authorized")
+    kisiClient
+            .get('fail')
+            .then(() => done(new Error('Should not happen')))
+            .catch((error) => {
+              assert.strictEqual(error.status, 401);
+              assert.strictEqual(error.code, 'abc123');
+              assert.strictEqual(error.reason, 'Not authorized');
 
-                done()
-            })
-    })
+              done();
+            });
+  });
 
-    it("should fail without reason", function (done) {
-        axiosMockAdapter
+  it('should fail without reason', (done) => {
+    axiosMockAdapter
             .onGet(/failWithoutReason$/)
-            .reply(401, null, {})
+            .reply(401, null, {});
 
-        kisiClient
-            .get("failWithoutReason")
-            .then(() => done(new Error("Should not happen")))
-            .catch(error => {
-                assert.strictEqual(error.status, 401)
-                assert.strictEqual(error.code, "000000")
+    kisiClient
+            .get('failWithoutReason')
+            .then(() => done(new Error('Should not happen')))
+            .catch((error) => {
+              assert.strictEqual(error.status, 401);
+              assert.strictEqual(error.code, '000000');
 
-                done()
-            })
-    })
-})
+              done();
+            });
+  });
+});
